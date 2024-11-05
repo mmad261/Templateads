048cf481df77147670fc095af3342010ebb0b51a
Matin Dehghanian <matindehghanian98@gmail.com>
2024-11-05 15:49:28 +0330

    remove path from router

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter
-    basename={`${
-      !import.meta.env?.VITE_PATH
-        ? window.location.pathname
-        : `/${import.meta.env?.VITE_PATH}`
-    }`}
-  >
+  <BrowserRouter>
     <App />
     <ToastContainer
       position="top-right"
